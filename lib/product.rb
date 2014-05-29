7cc4471c5e1042f8b0a987a3fb8d5b54bd131c0d
Sebastian Lugo <sebalugo@ubuntu.(none)>
2014-05-29 07:41:56 -0700

    Product class constructor, setters and getters

--- lib/product.rb
+++ lib/product.rb
@@ -1,8 +1,16 @@
 class Product
+        
+	attr_accessor :title,:price,:category
 
+	def initialize title,price,category
+
+	@title = title
+	@price = price
+	@category = category
+
+	end
 	# attribute names should be title, price, category
 	# where title is a String
 	# where price is a Float
 	# where category is a Symbol
-
 end
